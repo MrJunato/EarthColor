7acacdcb7353e273b65185213bdd5e8577b2d92e
Renato Junior <47343193+MrJunato@users.noreply.github.com>
2019-09-14 13:44:54 -0300

    Add files via upload

---
+++ EarthColor.R
@@ -0,0 +1,51 @@
+chooseCRANmirror(11)
+#https://secondnature.org/planet-earth-1401465698wt7/
+
+# install packages --------------------------------------------------------
+
+install.packages("imager")
+install.packages("ggplot2")
+
+
+# load packages -----------------------------------------------------------
+
+library(imager)
+library(ggplot2)
+
+# test image library ------------------------------------------------------
+
+plot(boats)
+x <- boats
+plot(x)
+
+# internet image ----------------------------------------------------------
+image <- load.image("earth.jpg")
+dim(image)
+plot(image)
+
+# Function to change the number of clusters -------------------------------
+new_color <- function(x){
+  #matrix_image <- matrix(image, nrow = 300*300, ncol = 3)
+  matrix_image <- array(image,dim=c(300*300,3))
+  colnames(matrix_image) <- c("Red","Green","Blue")
+  #plot(matrix_image)
+  #head(matrix_image)
+  
+  set.seed(99)
+  kmeans_image <- kmeans(matrix_image, x,iter.max=1000)
+  #str(kmeans_image)
+  #kmeans_image$centers
+  #kmeans_image$cluster
+  
+  for(i in 1:(300*300)){
+    cluster <- kmeans_image$cluster[i]
+    matrix_image[i,1:3] <-  kmeans_image$centers[cluster,1:3]
+  }
+  
+  new_image <- array(matrix_image,dim=c(300,300,1,3))
+  new_image<- cimg(new_image)
+  plot(new_image)
+}
+
+# Change function number to watch the difference in earth image -----------
+new_color(6)
